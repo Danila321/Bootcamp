0e88bd2d16d719eadfb14599a2792ee13f96fa2a
Danila321 <danil.hasanov220@gmail.com>
2024-07-02 15:56:21 +0300

    добавлен GameScreen

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,32 +1,63 @@
 package com.mygdx.game;
 
+import static com.mygdx.game.GameSettings.POSITION_ITERATIONS;
+import static com.mygdx.game.GameSettings.STEP_TIME;
+import static com.mygdx.game.GameSettings.VELOCITY_ITERATIONS;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.screens.GameScreen;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+public class MyGdxGame extends Game {
+
+	public World world;
+
+	public Vector3 touch;
+	public SpriteBatch batch;
+	public OrthographicCamera camera;
+
+	public GameScreen gameScreen;
+
+	float accumulator = 0;
+
 	@Override
-	public void create () {
+	public void create() {
+
+		Box2D.init();
+		world = new World(new Vector2(0, 0), true);
+
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
 
+		gameScreen = new GameScreen(this);
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		setScreen(gameScreen);
 	}
 
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+	}
+
+	public void stepWorld() {
+		float delta = Gdx.graphics.getDeltaTime();
+		accumulator += Math.min(delta, 0.25f);
+
+		if (accumulator >= STEP_TIME) {
+			accumulator -= STEP_TIME;
+			world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+		}
 	}
 }
