5e23e612154243703e7538653a55a15c7eaa4bf2
L1d0ff <viktorsbit10@gmail.com>
2024-07-02 15:24:37 +0300

    Игровое поле + размер экрана

--- core/src/com/mygdx/game/GameSettings.java
+++ core/src/com/mygdx/game/GameSettings.java
@@ -2,4 +2,6 @@ package com.mygdx.game;
 
 public class GameSettings {
     public static final float SCALE = 0.05f;
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
 }
