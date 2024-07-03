fafbe80de41eca5196ec5afd453cf525b8e74e0c
Euhell <evgenijreppo@gmail.com>
2024-07-03 15:19:09 +0300

    исправили путь

--- core/src/com/mygdx/game/BaseTower.java
+++ core/src/com/mygdx/game/BaseTower.java
@@ -1,8 +1,10 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class BaseTower {
 
-    public BaseTower(String texturePath, int x, int y, float width, float height) {
+    public BaseTower(String texturePath, int x, int y, float width, float height, World wpo) {
 
     }
 }
--- desktop/build/resources/main/map.tsx
+++ desktop/build/resources/main/map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="map" tilewidth="32" tileheight="32" margin="33" tilecount="735" columns="35">
- <image source="D:/Downloads/tileset_x1.png" width="1184" height="736"/>
+ <image source="tileset_x1.png" width="1184" height="736"/>
 </tileset>
