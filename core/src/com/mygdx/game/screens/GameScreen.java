3541796c041c5399c5b374eff56336d12cf80932
Euhell <evgenijreppo@gmail.com>
2024-07-03 14:43:08 +0300

    добавили карту

--- core/src/com/mygdx/game/screens/GameScreen.java
+++ core/src/com/mygdx/game/screens/GameScreen.java
@@ -17,44 +17,48 @@ import com.mygdx.game.gamefield.GameField;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.ui.ImageView;
 import com.mygdx.game.utility.GameResourses;
+import com.mygdx.game.utility.GameSettings;
 
 public class GameScreen extends ScreenAdapter {
     MyGdxGame myGdxGame;
-    GameField gameField;
-    ImageView bg;
+    //GameField gameField;
+    //ImageView bg;
     TiledMap tiledMap;
     private OrthogonalTiledMapRenderer tiledMapRenderer;
 
 
     public GameScreen(MyGdxGame myGdxGame) {
         this.myGdxGame = myGdxGame;
-        gameField = new GameField(16, 9);
-        bg = new ImageView(0, 0, GameResourses.BACK);
+        //gameField = new GameField(16, 9);
+        //bg = new ImageView(0, 0, GameResourses.BACK);
     }
 
     @Override
     public void show() {
         restartGame();
-
     }
 
     public void loadMap() {
         TmxMapLoader mapLoader = new TmxMapLoader();
         tiledMap = mapLoader.load("mapYesYes.tmx");
-        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 2f);
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap, GameSettings.MAP_SCALE);
     }
 
     @Override
     public void render(float delta) {
+        loadMap();
         draw();
         handleInput();
+
+        tiledMapRenderer.setView(myGdxGame.camera);
+        tiledMapRenderer.render();
     }
 
     private void handleInput() {
-        if (Gdx.input.isTouched()) {
+        if (Gdx.input.justTouched()) {
             //myGdxGame.touch = myGdxGame.camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
-            Vector2 touckPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-            if (hasObjectCoordinates("tower", touckPos)) {
+            Vector2 touchPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+            if (hasObjectCoordinates("tower", touchPos)) {
                 System.out.println("click");
             }
         }
@@ -64,7 +68,7 @@ public class GameScreen extends ScreenAdapter {
         MapObjects objects = tiledMap.getLayers().get(tower).getObjects();
         if (objects != null) {
             for (RectangleMapObject object : objects.getByType(RectangleMapObject.class)) {
-                if (object.getRectangle().contains(touchPos.x, 720 - touchPos.y)) {
+                if (object.getRectangle().contains(touchPos.x / GameSettings.MAP_SCALE, touchPos.y/ GameSettings.MAP_SCALE)) {
                     return true;
                 }
             }
--- core/src/com/mygdx/game/utility/GameSettings.java
+++ core/src/com/mygdx/game/utility/GameSettings.java
@@ -4,7 +4,7 @@ public class GameSettings {
     public static final float SCALE = 0.05f;
 
     // Device settings
-    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_WIDTH = 720;
     public static final int SCREEN_HEIGHT = 720;
 
     // Physics settings
@@ -14,4 +14,5 @@ public class GameSettings {
     public static final int POSITION_ITERATIONS = 6;
     public static final short ENEMY_BIT = 2;
     public static final short MAIN_HERO_BIT = 4;
+    public static final float MAP_SCALE = 2.25f;
 }
